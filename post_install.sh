413a022230ece951c3e09d12bc8c4b191820c786
onek2900 <98646377+onek2900@users.noreply.github.com>
2025-03-03 21:25:50 +0300

    Update post_install.sh

--- post_install.sh
+++ post_install.sh
@@ -4,22 +4,22 @@
 DB_NAME="main"
 ADMIN_PASS="Basilboss12"
 
-echo "Creating Odoo database and installing addons..."
+# echo "Creating Odoo database and installing addons..."
 
 # Run Odoo shell commands
-sudo -H -u odoo17 bash <<EOF
-cd /opt/odoo17
+# sudo -H -u odoo17 bash <<EOF
+# cd /opt/odoo17
 
 # Activate virtual environment
-source odoo17-venv/bin/activate
+# source odoo17-venv/bin/activate
 
 # Create database
-/opt/odoo17/odoo17-venv/bin/python3 /opt/odoo17/odoo17/odoo-bin --db_host=localhost --db_user=odoo17 --db_password=odoo17 --admin_passwd=$ADMIN_PASS -d $DB_NAME --stop-after-init --init=base
+# /opt/odoo17/odoo17-venv/bin/python3 /opt/odoo17/odoo17/odoo-bin --db_host=localhost --db_user=odoo17 --db_password=odoo17 --admin_passwd=$ADMIN_PASS -d $DB_NAME --stop-after-init --init=base
 
 echo "Database $DB_NAME created successfully!"
 
 
-deactivate
-EOF
+# deactivate
+# EOF
 
 echo "Post-installation tasks completed!"
